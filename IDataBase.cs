c417e872eeb8d2410550762f8d3fecc4d7026805
IoannTerrible <qwerty_qwerty12345678910@mail.ru>
2024-08-17 19:56:26 +0300

    Добавьте файлы проекта.

---
+++ IDataBase.cs
@@ -0,0 +1,21 @@
+﻿using SimpleTextDatabase;
+
+namespace WordDataBase
+{
+    public interface IDatabase
+    {
+        void BeginTransaction();
+        void CommitTransaction();
+        void RollbackTransaction();
+        void CreateTable(string tableName, Column[] columns);
+        void InsertData(string tableName, string[] data);
+        IEnumerable<string[]> Select(
+            string tableName,
+            string[] columns = null,
+            Func<string[], bool> filter = null,
+            string orderByColumn = null
+        );
+        void DropTable(string tableName);
+        void DropDatabase();
+    }
+}
